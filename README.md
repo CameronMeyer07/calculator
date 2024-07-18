3827b2bf167fc8fbfe5430cdacb0ce9686f380f5
Cameron <171266880+CameronMeyer07@users.noreply.github.com>
2024-07-18 18:07:28 +0200

    Add the working JS to the calculator

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ This is one of the hardest things I've tried in a long time.
 It's fun but also makes me feel unintelligent.
 I've been at this for a couple of weeks and thought I was getting somewhere.
 Then came this project.
+
+This was and is one of the hardest projects I have tried.
+Thinking I did good on the Etch-a-Sketch. Getting this project done
+had me scouring the internet for everything. 
+Looking up things I didn't know existed.
+I watched a couple of videos explaining different concepts.
+
+I still don't fully understand everything. 
+But as time goes on and I learn more I'm sure I will
+improve.
--- script.js
+++ script.js
@@ -1,21 +1,104 @@
-// Global variables
- let displayValue = '0';
- let firstVal = '';
- let secVal = '';
- let operator = '';
+class Calculator{
+    constructor(previousOpperandTextElement, currentOpperandTextElement){
+        this.previousOpperandTextElement = previousOpperandTextElement;
+        this.currentOpperandTextElement = currentOpperandTextElement;
+        this.clear();
+    }
 
-// functions
-const add = (a, b) => a + b;
-const sub = (a, b) => a - b;
-const divide = (a, b) => a / b;
-const multiply = (a, b) => a * b;
+    clear(){
+        this.currentOpperand = '';
+        this.previousOpperand = '';
+        this.operation = undefined;
+    }
 
-// update display
-const displayUpdate = () => {
-const display = document.querySelector(".display");
-display.innerText = displayValue;
+    delete(){
+        this.currentOpperand = this.currentOpperand.toString().slice(0, -1);
+    }
+
+    appendNumber(number){
+        if (number === '.' && this.currentOpperand.includes('.')) return;
+        this.currentOpperand += number;
+    }
+
+    chooseOpperation(operation){
+        if (this.currentOpperand === '') return;
+        if (this.previousOpperand !== ''){
+            this.compute()
+        }
+        this.operation = operation;
+        this.previousOpperand = this.currentOpperand;
+        this.currentOpperand = '';
+    }
+
+    compute(){
+        let computation ;
+        const prev = parseFloat(this.previousOpperand);
+        const current = parseFloat(this.currentOpperand);
+        if (isNaN(prev) || isNaN(current))return;
+        
+        switch(this.operation){
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;    
+            case '*':
+                computation = prev * current;
+                break;
+            case '/':
+                computation = prev / current;
+                break;
+            default: 
+                return;
+        }
+        this.currentOpperand = computation;
+        this.operation = undefined;
+        this.previousOpperand = '';
+    }
+
+    updateDisplay(){
+        this.currentOpperandTextElement.innerText = this.currentOpperand;
+        this.previousOpperandTextElement.innerText = this.previousOpperand;
+    }
 };
 
-// clear memory(reset)
 
-// store variables
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals');
+const deleteButton = document.querySelector('[data-delete');
+const allClearButton = document.querySelector('[data-all-clear');
+const previousOpperandTextElement = document.querySelector('[data-previous-opperand');
+const currentOpperandTextElement = document.querySelector('[data-current-opperand');
+
+const calculator = new Calculator(previousOpperandTextElement,  currentOpperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOpperation(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
